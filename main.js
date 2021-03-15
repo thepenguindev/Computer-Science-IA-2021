e3d2feb12b4e1f22f082750aefd0f0e57a9b109b
thepenguindev <theepicpenguin121@gmail.com>
2021-03-15 16:12:45 -0700

    Testing More

--- main.js
+++ main.js
@@ -4,5 +4,7 @@ let vec_one = new Vec2(0, 0);
 let vec_two = new Vec2(1, 1);
 
 console.log(vec_one);
+console.log(vec_one.x, vec_one.y);
 console.log(vec_two);
+console.log(vec_two.x, vec_two.y);
 console.log(vec_one.equals(vec_two));
