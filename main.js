932dc61b829c6acd2aa02bf8600f706c2ac59736
thepenguindev <theepicpenguin121@gmail.com>
2021-03-15 16:26:08 -0700

    I feel stupid

--- main.js
+++ main.js
@@ -2,10 +2,14 @@ import Note from './Objects/Note.mjs';
 
 let note = new Note(0, 0);
 
-const step = function() {
+const setup = function() {
     console.log(note.pos.x, note.pos.y);
 }
 
 const draw = function() {
 
 }
+
+// Exposing Browser to p5 functions because modules are wonky w/ p5
+//window.setup = setup;
+//window.draw = draw;
